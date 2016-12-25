01e91aaebd9df3d55217e0048cb3e702b7dcaa08
Rohit Saraogi <rohitsaraogi101@gmail.com>
2016-12-25 15:28:41 +0530

    Create Assignment_3_1.java

---
+++ Assignment_3_1.java
@@ -0,0 +1,21 @@
+ public class Assignment_3_1 {
+
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		static
+		{
+			print(10);
+		}
+		static void print(int x)
+		{
+			System.out.println(x);
+			System.exit(0);
+		}
+	}
+ } 
+/* IN THE given question there is no main method, if we compile the program
+ without main method, the program will compile but did not give output, it means
+ it gives runtime error.
+       but if we insert main method, the  program gives compile time error. 
+ */

8746351d20f4bb9035a5fa54feedb37a113c9b9c
Rohit Saraogi <rohitsaraogi101@gmail.com>
2016-12-25 15:32:00 +0530

    Create Assignment3_3.java

---
+++ Assignment3_3.java
@@ -0,0 +1,12 @@
+public class Assignment3_3 {
+	int x;     /* integer variable x*/
+	int y;     /* integer variable y */
+	String name;  /* String variable name */
+	public static void main(String args[])  /* main method */
+	{
+		Assignment3_3 pnt = new Example_Assignment3_3();  /* object is creating using new keyword */
+		System.out.println("pnt is = "+pnt.name+" "+pnt.x+" "+pnt.y); /* print the value of name, x and y */
+	}
+
+}
+	/* the The program prints pnt is = null 0 0. */
